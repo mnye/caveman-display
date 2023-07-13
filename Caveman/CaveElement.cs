6ccf9d70418836962bbec24fa4044ad9df884678
cbspace <24204459+cbspace@users.noreply.github.com>
2023-07-13 18:59:01 +1000

    Created CaveElement classes

---
+++ Caveman/CaveElement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Some classes to store the Cave Elements
+// Uses inheritence for each element type
+
+namespace Caveman
+{
+    enum ElementType
+    {
+        Line,
+        Text
+    }
+
+    internal abstract class CaveElement
+    {
+        public ElementType elementType;
+        public Point startLocation;
+        
+        public CaveElement(ElementType my_elementType, Point my_startLocation) 
+        {
+            elementType = my_elementType;
+            startLocation = my_startLocation;
+        }
+
+        public abstract String GenerateCpp();
+    }
+
+    class LineElement : CaveElement
+    {
+        public Point endLocation;
+        public LineElement(Point my_startLocation) 
+            : base(ElementType.Line, my_startLocation)
+        { 
+            endLocation = my_startLocation;
+        }
+
+        public override string GenerateCpp() 
+        {
+            return "display.drawLine(BLACK);";
+        }
+    }
+    class TextElement : CaveElement
+    {
+        public TextElement(Point my_startLocation)
+            : base(ElementType.Text, my_startLocation)
+        {
+
+        }
+
+        public override string GenerateCpp()
+        {
+            return "hello, I am a text object :)";
+        }
+    }
+}
